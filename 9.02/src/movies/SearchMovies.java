8282cad1ea9a8acc3fcac6b2e005cb05d4d53595
Brad Bodily <bod12002@byui.edu>
2014-11-18 07:19:59 -0700

    Added some basic structure/comments to SearchMovies

--- 9.02/src/movies/SearchMovies.java
+++ 9.02/src/movies/SearchMovies.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Servlet implementation class SearchMovies
  */
@@ -28,12 +30,43 @@ public class SearchMovies extends HttpServlet implements Servlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Get Search Results from OMDb API
+		// Get Results from OMDb API
 		
 		// Get parameters from form
 		String title = request.getParameter("title");
-		String view = request.getParameter("view");		// will be set to 'detail' if we need to get details for a specific movie
+		String view = "search";									// will be set to 'detail' if we need to get details for a specific movie
 		
+        if (request.getParameterMap().containsKey("view")) {
+            view = request.getParameter("view");
+        }
+		
+		switch (view)
+		{
+			// Get specific movie details
+			case "detail":
+				// TODO: Get movie details from API
+				
+				// Show Detail view
+				request.getRequestDispatcher("/detail.jsp").forward(request, response);				
+				break;
+
+			// Search for movies based off title string
+			default:
+				// TODO: Get search results from API
+				
+
+				// TODO: Uncomment the line below to pass 'results'
+				// to the search form as a parameter named "results"
+				// that it will use to display the search results
+				// (assuming we store our search results in a 'results'
+				// variable.
+
+				// request.setAttribute("results", results);
+				
+				// Show Search view
+				request.getRequestDispatcher("/search.jsp").forward(request, response);
+				break;
+		}
 		
 	}
 
