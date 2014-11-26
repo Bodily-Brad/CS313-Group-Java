b5651265f302e40a4a2403d230124c8baabb5766
cvergaray <ver08007@byui.edu>
2014-11-25 20:14:07 -0700

    Create DatabaseInteractor.java
    This code is untested because I have not been able to get my MySql database running locally and my OpenShift server is down. I am not sure why, but if anyone else can test it, that would be wonderful

---
+++ 10.02/src/DatabaseInteractor/DatabaseInteractor.java
@@ -0,0 +1,138 @@
+package databaseInteractor;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Person;
+
+public class DatabaseInteractor {
+
+	static final String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+	static final String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+
+	// JDBC driver name and database URL
+	private String DB_URL;
+	private String database = "";
+
+	// Database credentials
+	private String USER = "team";
+	private String PASS = "pass";
+
+	Connection conn = null;
+	Statement stmt = null;
+
+	public DatabaseInteractor() {
+		selectDatabase("/familyDB");
+		DB_URL = "jdbc:mysql://" + host + ":" + port;
+	}
+
+	public void selectDatabase(String dbName) {
+		if (!dbName.startsWith("/"))
+			dbName = "/" + dbName;
+		database = dbName;
+	}
+
+	public boolean connectToDatabase(String dbName) {
+		selectDatabase(dbName);
+		return connectToDatabase();
+	}
+	
+	public void useLocalDB(){
+		DB_URL = "jdbc:mysql://localhost";
+	}
+
+	public boolean connectToDatabase() {
+		boolean success = true;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			conn = DriverManager.getConnection(DB_URL + database, USER, PASS);
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+			success = false;
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+			success = false;
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+		return success;
+	}
+
+	public List<Person> executeQuery(String query) {
+
+		List<Person> people = new ArrayList<Person>();
+		ResultSet rs = null;
+
+		if (conn == null)
+			connectToDatabase();
+		try {
+
+			// STEP 4: Execute a query
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				// Get info to create Person object
+				int personID = rs.getInt("person_id");
+				String first = rs.getString("first_name");
+				String last = rs.getString("last_name");
+				Date birth = rs.getDate("birthday");
+
+				// Create new person
+				Person newPerson = new Person(personID, first, last,
+						birth.toString());
+
+				// Add person to return list
+				people.add(newPerson);
+			}
+			// Clean-up environment
+			stmt.close();
+			conn.close();
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+
+		return people;
+	}
+}
