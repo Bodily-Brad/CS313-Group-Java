0d6988720387dd34c5f7f2a4e9a7e789c4b66662
Bodily-Brad <bod12002@byui.edu>
2014-12-05 16:39:32 -0700

    Added Location class

--- GuessingGame/src/models/Location.java
+++ GuessingGame/src/models/Location.java
@@ -1,8 +1,36 @@
 package models;
 
 public class Location {
+	
 	// Local Variables
 	double latitude;
 	double longitude;
+	
+	// Constructor
+	public Location(double latitude, double longitude)
+	{
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public Location()
+	{
+		this.latitude = 0.0;
+		this.longitude = 0.0;
+	}
+	
+	// Properties
+	public double getLatitude() { return latitude; }
+	public void setLatitude(double latitude) { this.latitude = latitude; }
+	
+	public double getLongitude() { return longitude; }
+	public void setLongitude(double longitude) { this.longitude = longitude; }
+	
+	// Methods
+	@Override
+	public String toString()
+	{
+		return "(" + latitude + "," + longitude + ")";
+	}
 
 }
